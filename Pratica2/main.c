3651f9a22aa1873ca2d41ccd81c38e3036b37ad9
Alexandre <alexandrer0x@hotmail.com>
2015-04-01 23:53:25 -0300

    Atualizacao pratica 2

--- Pratica2/main.c
+++ Pratica2/main.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
-int fibonacci3(int n){
+double calcula(clock_t fim, clock_t ini){
+    return ((double) (fim - ini) / CLOCKS_PER_SEC);
+}
+
+int fibonacci1(int n){
     if(n == 0)
         return 0;
     if(n == 1)
         return 1;
     else
-        return (fibonacci3(n-1) + fibonacci3(n-2));
+        return (fibonacci1(n-1) + fibonacci1(n-2));
 }
 
-
-
 int fibonacci2(int n){
+    int i = 2;
+    int v[50];
+
+    v[0] = 0;
+    v[1] = 1;
+
+    while(i <= n){
+        v[i] = v[i-1] + v[i-2];
+        i++;
+    }
+
+    return v[n];
+}
+
+int fibonacci3(int n){
     double a1, a2, r;
 
     a1 = pow((1 + (sqrt(5))) / 2, n);
@@ -23,32 +41,33 @@ int fibonacci2(int n){
 }
 
 
-int fibonacci1(int n){
-    int i = 2;
-    int v[50];
-
-    v[0] = 0;
-    v[1] = 1;
-
-    while(i <= n){
-        v[i++] = v[i-1] + v[i-2];
-    }
-
-    return v[n];
-}
-
-
 int main(){
-    int fb1, fb2, fb3, n;
+    int fb, n;
+    clock_t t1, t2;
+    double total;
 
     printf("Digite o N: ");
     scanf("%d", &n);
 
-    fb1 = fibonacci1(n);
-    printf("\nMetdo #1 Fibonacci %d = %d\n\n", n, fb1);
-    fb2 = fibonacci2(n);
-    printf("Metdo #2 Fibonacci %d = %d\n\n", n, fb2);
-    fb3 = fibonacci3(n);
-    printf("Metdo #3 Fibonacci %d = %d\n\n", n, fb3);
+    t1 = clock();
+    fb = fibonacci1(n);
+    t2 = clock();
+    total = calcula(t2, t1);
+    printf("\nMetodo #1 Fibonacci %d = %d\nDuracao de %.3lf seg.\n\n", n, fb, total);
+
+    t1 = clock();
+    fb = fibonacci2(n);
+    t2 = clock();
+    total = calcula(t2, t1);
+    printf("\nMetodo #2 Fibonacci %d = %d\nDuracao de %.3lf seg.\n\n", n, fb, total);
+
+    t1 = clock();
+    fb = fibonacci3(n);
+    t2 = clock();
+    total = calcula(t2, t1);
+    printf("\nMetodo #3 Fibonacci %d = %d\nDuracao de %.3lf seg.\n\n", n, fb, total);
+
+
+
     return 0;
 }
